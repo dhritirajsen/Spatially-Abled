7dd71389cbbac158424e7d50c6f25eb71b40b234
Arijit <minhack@Arijits-MacBook-Air.local>
2021-12-12 22:51:25 +0530

    feat(blog): added blog section

---
+++ blog_data.js
@@ -0,0 +1,25 @@
+const blogBody = [
+    {
+        "blog_no": 1,
+        "title": "Working with QA Bands and Bitmasks in Google Earth Engine",
+        "image_location": "./image/resource/CFC_1.jpg",
+        "is_image": true,
+        "body": `
+        Most optical satellite imagery products come with one or more QA-bands that allows the user to assess quality of each pixel and extract pixels that meet their requirements. The most common application for QA-bands is to extract information about cloudy pixels and mask them. But the QA bands contain a wealth of other information that can help you remove low quality data from your analysis. Typically the information contained in QA bands is stored as Bitwise Flags. In this post, I will cover basic concepts related to Bitwise operations and how to extract and mask with specific quality indicators using Bitmasks.
+        For this post, we will work with MOD11A1.006 Terra Land Surface Temperature and Emissivity Daily Global 1km dataset. But the concepts and code snippet can be applied to any other dataset easily. Looking at the bands metadata, we see that the LST_Day_1km band is accompanied by a QC_Day band containing LST Quality Indicators.
+        <img src="./image/resource/CFC_1.jpg" width="350px" height="220px" alt="Bitmasks" >
+        `,
+        "introdcution": ""
+    },
+    {
+        "blog_no": 2,
+        "title": "some Title",
+        "is_image": false,
+        "body": "body",
+        "introdcution": "introduction blog 2"
+    }
+]
+
+function returnBlog(){
+    return blogBody
+}
